3bdb6fc888dc3a45ef12be591e19e726784adf73
Aneri3112 <aneri.pate@hotmail.com>
2022-07-07 23:07:52 +0200

    Add chat UI

--- components/Chat.js
+++ components/Chat.js
@@ -1,18 +1,66 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, View, Text} from 'react-native';
+import React, { useEffect, useState, useCallback } from 'react';
+import { StyleSheet, View, Platform, KeyboardAvoidingView} from 'react-native';
+//import GiftedChat
+import { GiftedChat, Bubble } from 'react-native-gifted-chat'
 
 export default function Chat(props) {
     let { name, backgroundColor } = props.route.params;
+    const [messages, setMessages] = useState([]);
 
-    // Set the screen title to the user name entered in the start screen
+  // Set the screen title to the user name entered in the start screen
     useEffect(() => {
       props.navigation.setOptions({ title: name });
+      setMessages([
+        {
+          _id: 1,
+          text: 'Hello Developer',
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://i.pinimg.com/564x/51/3c/27/513c27c5cfa8d1b65a60835764fd38d6.jpg',
+          },
+        },
+        {
+          _id: 2,
+          text: `${name} has entered the chat.`,
+          createdAt: new Date(),
+          system: true,
+        },
+      ]);
     }, [])
     
 
+    const onSend = useCallback((messages = []) => {
+      setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+    }, [])
+
+    //Customize sender bubble color
+    const renderBubble = (props) => {
+      return (
+        <Bubble
+          {...props}
+          wrapperStyle={{
+            right: {
+              backgroundColor: '#000'
+            }
+          }}
+        />
+      )
+    }
+
     return (
       <View style={[{ backgroundColor: backgroundColor}, styles.conatainer]}>
-        <Text style={styles.text}>Welcome to Chat App!</Text>
+        <GiftedChat
+          renderBubble={renderBubble.bind()}
+          messages={messages}
+          onSend={messages => onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />  
+      
+        {/* Avoid keyboard to overlap text messages on older Andriod versions */}
+        {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
       </View>
     )
   }
@@ -20,11 +68,5 @@ export default function Chat(props) {
   const styles = StyleSheet.create({
     conatainer: {
       flex:1, 
-      justifyContent: "center", 
-      alignItems: 'center',
-    },
-
-    text: {
-      color: 'white'
     },
   })
